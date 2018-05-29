998927e20f4664728c2339a90b56b9ec92fb7be3
Luisa <Luisa@192.168.56.1>
2018-05-29 14:47:57 -0600

    Interfaz de la ventana de la calculadora

--- src/Main/Calculadora_.java
+++ src/Main/Calculadora_.java
@@ -5,6 +5,11 @@
  */
 package Main;
 
+import GUI.Ventana;
+import Main.Aritmetica.Aritmetico;
+import Main.Convertor.Convertor;
+import javax.swing.JFrame;
+
 /**
  *
  * @author LN710Q
@@ -12,7 +17,13 @@ package Main;
 public class Calculadora_ {
 
     public static void main(String[] args) {
-        
+
+        JFrame ventana = new JFrame("Calculadora Basica");
+        ventana.setContentPane(new Ventana());
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventana.setResizable(false);
+        ventana.pack();
+        ventana.setVisible(true);
     }
     
 }
